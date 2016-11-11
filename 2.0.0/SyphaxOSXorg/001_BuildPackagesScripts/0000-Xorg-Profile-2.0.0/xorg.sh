53575930c1b2254830cc0f5212d6e1b2d19c9de6
hmasmoudi <hatem.masmoudi@groupe-telnet.net>
2016-11-11 23:34:02 +0100

    Update xorg.sh build env

--- 2.0.0/SyphaxOSXorg/001_BuildPackagesScripts/0000-Xorg-Profile-2.0.0/xorg.sh
+++ 2.0.0/SyphaxOSXorg/001_BuildPackagesScripts/0000-Xorg-Profile-2.0.0/xorg.sh
@@ -1,4 +1,3 @@
-XORG_PREFIX="$XORG_PREFIX"
-XORG_CONFIG="--prefix=\$XORG_PREFIX --sysconfdir=/etc
---localstatedir=/var --disable-static"
+XORG_PREFIX="/usr"
+XORG_CONFIG="--prefix=$XORG_PREFIX --sysconfdir=/etc --localstatedir=/var --disable-static"
 export XORG_PREFIX XORG_CONFIG
