cffe74516a92a216ef161150a173d31accf15abb
hmasmoudi <hatem.masmoudi@groupe-telnet.net>
2016-11-14 00:52:11 +0100

    rename packages repo for 2.0.0

--- 2.0.0/SyphaxOSCore/001_BuildPackagesScripts/0071-core-configs-2.0.0/skel_vimrc
+++
@@ -1,7 +0,0 @@
-" Begin .vimrc
-
-set columns=80
-set wrapmargin=8
-set ruler
-
-" End .vimrc
--- 2.0.0/SyphaxOSXorg/001_BuildPackagesScripts/0000-Xorg-Profile-2.0.0/xorg.sh
+++
@@ -1,3 +0,0 @@
-XORG_PREFIX="/usr"
-XORG_CONFIG="--prefix=$XORG_PREFIX --sysconfdir=/etc --localstatedir=/var --disable-static"
-export XORG_PREFIX XORG_CONFIG
