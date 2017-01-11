f9a9c27237da2d861d5df5ea2e067fcab5bfba7b
hmasmoudi <hatem.masmoudi@gmail.com>
2017-01-11 20:07:00 +0100

    Move extra package from application to extra

--- Default/0006-SyphaxOS-Extra/001_BuildPackagesScripts/0010-SyphaxOS-DVD-Maker-2.0.0/SyphaxOS-DVD-Maker/makeSyphaxOS-DVD.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DVD_NAME=SyphaxOS-DVD-2.0.0_Beta1-x86_64.iso
-
-rm -rf $DVD_NAME
-mkdir -pv SyphaxOS
-cp -rf live SyphaxOS/
-cp -rf boot SyphaxOS/
-/usr/bin/grub-mkrescue -d /usr/lib/grub/i386-pc --product-name=SyphaxOS --product-version=2.0.0-Beta1  -o $DVD_NAME SyphaxOS
