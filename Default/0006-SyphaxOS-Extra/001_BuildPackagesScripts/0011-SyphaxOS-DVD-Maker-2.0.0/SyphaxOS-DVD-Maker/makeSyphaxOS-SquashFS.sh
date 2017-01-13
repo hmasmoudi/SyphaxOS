c10a9ec3015a3db34e6a61d73c061b5ce11370b9
hmasmoudi <hatem.masmoudi@gmail.com>
2017-01-13 20:25:31 +0100

    Update Installer Script to prevent the copy of installed Package in Memory

--- Default/0006-SyphaxOS-Extra/001_BuildPackagesScripts/0011-SyphaxOS-DVD-Maker-2.0.0/SyphaxOS-DVD-Maker/makeSyphaxOS-SquashFS.sh
+++ Default/0006-SyphaxOS-Extra/001_BuildPackagesScripts/0011-SyphaxOS-DVD-Maker-2.0.0/SyphaxOS-DVD-Maker/makeSyphaxOS-SquashFS.sh
@@ -7,4 +7,4 @@ mkdir -pv SyphaxOS/live
 
 cd /SyphaxOS-2.0.0-DVD
 
-mksquashfs dev proc run sys bin boot etc home lib lib64 media mnt opt root sbin srv usr var tmp "$CMD"/SyphaxOS/live/syphaxos.sqsh
+mksquashfs dev proc run sys bin boot etc home lib lib64 media mnt opt root sbin srv usr var tmp Packages "$CMD"/SyphaxOS/live/syphaxos.sqsh
